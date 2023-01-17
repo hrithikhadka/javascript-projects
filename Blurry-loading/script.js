db34e8bc4cbb6646d2419266c66c86f8adc8939e
Hrithik Khadka <hritik.khadka@centria.fi>
2023-01-17 21:38:55 +0545

    make background opacity according to load percent

--- Blurry-loading/script.js
+++ Blurry-loading/script.js
@@ -9,4 +9,10 @@ function blurring() {
     clearInterval(int);
   }
   loadingText.innerHTML = `${load}%`;
+  loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 20, 0)}px)`;
 }
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
